89e129d74968e893becdfe9f357b09789d3e5a69
Werbison Oliveira <werbison090@gmail.com>
2025-10-13 23:37:13 -0300

    Configurando react-router

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -18,7 +18,7 @@ import FormChangePassword from '../components/FormChangePassword/FormChangePassw
 
 export const router = createBrowserRouter([
     {
-        path: "/",
+        path: "/ExpenseControl",
         element: <App />,
         children: [
             {
